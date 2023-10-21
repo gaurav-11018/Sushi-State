d160ddf6afb0de795e371bb360979be5a52bb1c8
shreyas0924 <shreyasp0924@gmail.com>
2023-10-21 10:27:40 +0530

    logo added

--- socialfiv2/app/(pages)/notifications/page.tsx
+++ socialfiv2/app/(pages)/notifications/page.tsx
@@ -1,7 +1,26 @@
+import Link from 'next/link'
 import React from 'react'
 
-const Notifications = () => {
-  return <div>Notifications page</div>
+const Notification = () => {
+  return (
+    <div className='p-4'>
+      <div className='max-w-md p-4 mt-4 rounded-lg shadow-md'>
+        <p className='text-xl'>Thank you for joining us</p>
+        <div className='p-2 mt-4 rounded'>
+          <h1>
+            To be eligible for a red envelope, click here to create your
+            <a
+              href='https://okto.tech/'
+              className='text-blue-400 mx-1'
+              target='_blank'
+            >
+              Okto Wallet.
+            </a>
+          </h1>
+        </div>
+      </div>
+    </div>
+  )
 }
 
-export default Notifications
+export default Notification
